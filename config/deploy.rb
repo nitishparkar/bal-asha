3afd78c0cb969f90826b4e9ad6163c4f7fc9e2d0
Nitish Parkar <developer.nitish@gmail.com>
2014-12-14 15:05:37 +0530

    figaro devise deploy debugging

--- config/deploy.rb
+++ config/deploy.rb
@@ -61,14 +61,14 @@ namespace :figaro do
   desc "SCP transfer figaro configuration to the shared folder"
   task :setup do
     on roles(:app) do
-      upload! "config/application.yml", "#{shared_path}/application.yml", via: :scp
+      upload! "config/application.yml", "#{shared_path}/config/application.yml", via: :scp
     end
   end
 
   desc "Symlink application.yml to the release path"
   task :symlink do
     on roles(:app) do
-      execute "ln -sf #{shared_path}/application.yml #{current_path}/config/application.yml"
+      execute "ln -sf #{shared_path}/config/application.yml #{current_path}/config/application.yml"
     end
   end
 end
