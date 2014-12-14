a5b10657295628e6e545a33230bb2cfeda689c93
Nitish Parkar <developer.nitish@gmail.com>
2014-12-14 15:14:20 +0530

    create config directory if not exists

--- config/deploy.rb
+++ config/deploy.rb
@@ -61,6 +61,7 @@ namespace :figaro do
   desc "SCP transfer figaro configuration to the shared folder"
   task :setup do
     on roles(:app) do
+      execute :mkdir, '-p', "#{shared_path}/config"
       upload! "config/application.yml", "#{shared_path}/config/application.yml", via: :scp
     end
   end
