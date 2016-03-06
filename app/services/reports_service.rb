59d2da820f9bbc2ab008b0a4db3144f2c8a00ed2
Nitish Parkar <nitish@getsimpl.com>
2016-03-06 14:05:05 +0530

    Handle missing category_id for total kind donations report

--- app/services/reports_service.rb
+++ app/services/reports_service.rb
@@ -1,8 +1,8 @@
 class ReportsService
 
   def self.total_kind_donations(start_date, end_date, category_id)
-    category = Category.find(category_id)
     if start_date && end_date && category_id
+      category = Category.find(category_id)
       TransactionItem.joins(:donation).includes(:item)
           .where(donations: { date: start_date.beginning_of_day..end_date.end_of_day }, item_id: category.item_ids)
           .group_by(&:item_id)
