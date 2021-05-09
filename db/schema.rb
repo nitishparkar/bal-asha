f308ad5cd2a83a68a95631802954e933dc0c5942
Nitish Parkar <developer.nitish@gmail.com>
2021-05-09 09:15:21 +0530

    Add identification fields

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201101154009) do
+ActiveRecord::Schema.define(version: 20210509031952) do
 
   create_table "call_for_actions", force: true do |t|
     t.datetime "date_of_action"
@@ -113,6 +113,8 @@ ActiveRecord::Schema.define(version: 20201101154009) do
     t.string   "country_code"
     t.datetime "deleted_at"
     t.integer  "status",                       limit: 4,     default: 0
+    t.integer  "identification_type",          limit: 1,     default: 0
+    t.string   "identification_no",            limit: 255
   end
 
   add_index "donors", ["country_code"], name: "index_donors_on_country_code", using: :btree
