309d6b5622cdb322c1039364f4ed1acdae3a79da
Nitish Parkar <developer.nitish@gmail.com>
2023-12-06 19:09:25 +0530

    Add service restart on mina launch

--- config/deploy.rb
+++ config/deploy.rb
@@ -65,6 +65,7 @@ task :deploy => :environment do
     to :launch do
       queue "mkdir -p #{deploy_to}/#{current_path}/tmp/"
       queue "touch #{deploy_to}/#{current_path}/tmp/restart.txt"
+      command "sudo systemctl restart bal-asha"
     end
   end
 end
