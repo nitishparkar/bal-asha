63b516c95b74dcf7e27f909e234a4887f1a271c7
Nitish Parkar <developer.nitish@gmail.com>
2024-09-02 21:31:48 +0530

    Run specs in random order
    and fix flaky specs

--- spec/factories/categories_factory.rb
+++ spec/factories/categories_factory.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
   factory :category, class: Category do
-    name { Faker::Commerce.department }
+    name { Faker::Commerce.department(3, true) } # Always combine 3 names
   end
 end
